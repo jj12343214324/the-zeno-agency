89de5a568a776eb9e61311fc9b685b86f72fa5a3
jj12343214324 <81502880+jj12343214324@users.noreply.github.com>
2026-01-14 17:14:32 -0700

    Move CNA section below CTA with white background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -235,19 +235,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* CNA Partner */}
-      <section className="py-8 bg-gray-50">
-        <div className="container-max flex justify-center">
-          <Image
-            src="/images/cna.png"
-            alt="CNA Insurance"
-            width={150}
-            height={75}
-            className="object-contain"
-          />
-        </div>
-      </section>
-
       {/* Why Choose Us Section */}
       <WhyChooseUs />
 
@@ -263,6 +250,19 @@ export default function Home() {
           <Link href="/contact" className="inline-block bg-white text-[#0F3E54] px-8 py-4 font-semibold text-lg hover:bg-gray-100 transition-colors uppercase tracking-wider">
             Request a Consultation
           </Link>
+        </div>
+      </section>
+
+      {/* CNA Partner */}
+      <section className="py-8 bg-white">
+        <div className="container-max flex justify-center">
+          <Image
+            src="/images/cna.png"
+            alt="CNA Insurance"
+            width={150}
+            height={75}
+            className="object-contain"
+          />
         </div>
       </section>
     </>
